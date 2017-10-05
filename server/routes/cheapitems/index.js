cfc143c308ecaeb222fd114b1b6733f5e8d93d6f
Albert Chung <aychung86 (at) gmail.com>
2017-10-05 10:17:39 -0700

    (feat) implement get deals and get cheap items

--- server/routes/cheapitems/index.js
+++ server/routes/cheapitems/index.js
@@ -1,27 +1,52 @@
+const cDB = require('../../../database');
+
 const cheapitemsRoutes = (app) =>{
 
   app.get('/cheapitems', (req, res) => {
     console.log('list cheapitems');
     // query cheap items table and get 25 items  (how should we sort?)
-    // res.send(cheapitems);
-    res.send('ok');
+    cDB.getCheapItems()
+      .then(cheapItems => {
+        res.send(cheapItems);
+      }).catch(err => {
+        console.log('GET cheapitems error!!!\n', err);
+        res.redirect('/');
+      }); 
   });
 
   app.get('/cheapitems/:zip', (req, res) => {
     console.log(`get cheap items at ${req.params.zip}`);
     // query cheap items table with ZIP and get 25 items  (how should we sort?)
-    // res.send(cheapitems)
-    res.send('ok');
+    cDB.getCheapItems(req.params.zip)
+      .then(cheapItems => {
+        res.send(cheapItems);
+      }).catch(err => {
+        console.log('GET cheapitems error!!!\n', err);
+        res.redirect('/');
+      }); 
   });
 
   app.get('/cheapitems/:zip/:cuisineType', (req, res) => {
     console.log(`get cheap items at ${req.params.zip} and type ${req.params.cuisineType}`);
     // query cheap items table with ZIP and cuisineType, and get 25 items  (how should we sort?)
-    // res.send(cheapitems);
-    res.send('ok');
+    cDB.getCheapItems(req.params.zip, req.params.cuisineType)
+      .then(cheapItems => {
+        res.send(cheapItems);
+      }).catch(err => {
+        console.log('GET cheapitems error!!!\n', err);
+        res.redirect('/');
+      }); 
   });
 
 
 }
 
 module.exports = cheapitemsRoutes;
+
+
+//////////////////////
+// list of database query request from server
+/*
+getCheapItems(zip = null, cuisineType = null)
+expect to return promise object with list of cheapItems
+*/
--- server/routes/deals/index.js
+++ server/routes/deals/index.js
@@ -1,26 +1,53 @@
+const cDB = require('../../../database');
+
 const dealsRoutes = (app) =>{
 
   app.get('/deals', (req, res) => {
+    console.log(cDB);
+    console.log(cDB.getDeals);
     console.log('get deals');
     // query deals table and get 25 deals
-    // res.send(deals)
-    res.send('ok');
+    cDB.getDeals()
+      .then(deals => {
+        console.log(deals);
+        res.send(deals);
+      }).catch(err => {
+        console.log('GET deals error');
+      });
   })
 
   app.get('/deals/:zip', (req, res) => {
     console.log(`get deals at ${req.params.zip}`);
     // query deals with ZIP table, and get 25 deals
-    // res.send(deals)
-    res.send('ok');
+    cDB.getDeals(req.params.zip)
+      .then(deals => {
+        console.log(deals);
+        res.send(deals);
+      }).catch(err => {
+        console.log('GET deals error');
+      });
   });
 
   app.get('/deals/:zip/:cuisineType', (req, res) => {
     console.log(`get deals at ${req.params.zip} and type ${req.params.cuisineType}`);
     // query deals table with ZIP and cuisineType, and get 25 deals
-    // res.send(deals)
-    res.send('ok');
+    cDB.getDeals(req.params.zip, req.params.cuisineType)
+      .then(deals => {
+        console.log(deals);
+        res.send(deals);
+      }).catch(err => {
+        console.log('GET deals error');
+      });
   });
 
 }
 
 module.exports = dealsRoutes;
+
+
+////////
+// list of database query request from server
+/*
+getDeals(zip = null, cuisineType = null) 
+expect to return promise object with list of deals
+*/
