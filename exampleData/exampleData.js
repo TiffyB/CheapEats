d688f7a50bb13fd343e0d3d2fc0da4a33c6e3845
Roscoe Coney <scoe@Roscoes-MacBook-Pro.local>
2017-10-04 19:30:08 -0700

    update with react

---
+++ exampleData/exampleData.js
@@ -0,0 +1,60 @@
+var fakeData = [
+{
+	restaurantName: "Roscoe's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Jim's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Jon's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Helen's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Helen's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Roscoe's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Rhonda's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+{
+	restaurantName: "Tami's waffles",
+	price: 5.23,
+	location: 123 north front st.,
+	expirationDate: '10/4/2017'
+},
+
+];
+
+
