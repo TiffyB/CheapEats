867183a135d1b034a770d92ffb1600eb3c94e401
ChristineETaylor <christine.e.taylor.sf@gmail.com>
2017-10-05 19:24:10 -0700

    (fix) Fixes PostgreSQL syntax and datatypes
    Tiff's fixes for command-line PostgreSQL creation

--- schema.sql
+++ schema.sql
@@ -1,28 +1,18 @@
--- ---
--- Globals
--- ---
-
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
-
--- ---
--- Table 'Deals'
--- 
--- ---
-
-DROP TABLE IF EXISTS Deals;
+DROP TABLE IF EXISTS Deals cascade;
 		
-CREATE SEQUENCE Deals_seq;
+CREATE SEQUENCE IF NOT EXISTS Deals_seq;
 
 CREATE TABLE Deals (
-  id INTEGER NULL DEFAULT NEXTVAL ('Deals_seq') DEFAULT NULL,
-  yelp_ID INTEGER NULL DEFAULT NULL,
-  price DECIMAL(3, 2) NOT NULL DEFAULT NULL,
-  dealName VARCHAR(50) NOT NULL DEFAULT 'NULL',
-  description MEDIUMTEXT(250) NULL DEFAULT 'NULL',
-  imageURL VARCHAR(100) NOT NULL DEFAULT 'NULL',
-  startTime DATETIME(100) NOT NULL DEFAULT 'NULL',
-  endTime DATETIME(100) NOT NULL DEFAULT 'NULL',
+  id INTEGER NOT NULL DEFAULT NEXTVAL ('Deals_seq'),
+  yelp_ID INTEGER,
+  price DECIMAL(3, 2),
+  dealName VARCHAR(50),
+  description TEXT,
+  imageURL VARCHAR(100),
+  startDate DATE,
+  startTime TIME,
+  endDate DATE,
+  endTime TIME,
   PRIMARY KEY (id)
 );
 
@@ -31,17 +21,17 @@ CREATE TABLE Deals (
 -- 
 -- ---
 
-DROP TABLE IF EXISTS CheapItems;
+DROP TABLE IF EXISTS CheapItems cascade;
 		
-CREATE SEQUENCE CheapItems_seq;
+CREATE SEQUENCE IF NOT EXISTS CheapItems_seq;
 
 CREATE TABLE CheapItems (
-  id INTEGER NULL DEFAULT NEXTVAL ('CheapItems_seq') DEFAULT NULL,
-  yelp_ID INTEGER NULL DEFAULT NULL,
-  price DECIMAL(3, 2) NOT NULL DEFAULT NULL,
-  menuItem VARCHAR(100) NOT NULL DEFAULT 'NULL',
-  imageURL VARCHAR(100) NULL DEFAULT 'NULL',
-  description MEDIUMTEXT(250) NULL DEFAULT 'NULL',
+  id INTEGER NOT NULL DEFAULT NEXTVAL ('CheapItems_seq'),
+  yelp_ID INTEGER,
+  price DECIMAL(3, 2),
+  menuItem VARCHAR(100),
+  imageURL VARCHAR(100),
+  description TEXT,
   PRIMARY KEY (id)
 );
 
@@ -50,14 +40,14 @@ CREATE TABLE CheapItems (
 -- 
 -- ---
 
-DROP TABLE IF EXISTS Owners;
+DROP TABLE IF EXISTS Owners cascade;
 		
-CREATE SEQUENCE Owners_seq;
+CREATE SEQUENCE IF NOT EXISTS Owners_seq;
 
 CREATE TABLE Owners (
-  id INTEGER NULL DEFAULT NEXTVAL ('Owners_seq') DEFAULT NULL,
-  login CHAR(30) NOT NULL DEFAULT 'NULL',
-  password CHAR(60) NOT NULL DEFAULT 'NULL',
+  id INTEGER NOT NULL DEFAULT NEXTVAL ('Owners_seq'),
+  login CHAR(30),
+  password CHAR(60),
   PRIMARY KEY (id)
 );
 
@@ -66,19 +56,19 @@ CREATE TABLE Owners (
 -- 
 -- ---
 
-DROP TABLE IF EXISTS YelpData;
+DROP TABLE IF EXISTS YelpData cascade;
 		
-CREATE SEQUENCE YelpData_seq;
+CREATE SEQUENCE IF NOT EXISTS YelpData_seq;
 
 CREATE TABLE YelpData (
-  id INTEGER NULL DEFAULT NEXTVAL ('YelpData_seq') DEFAULT NULL,
-  yelp_api_ID VARCHAR(100) NOT NULL DEFAULT NULL,
-  address MEDIUMTEXT(50) NOT NULL DEFAULT 'NULL',
-  ZIP CAST(5 AS INT) NOT NULL DEFAULT NULL,
-  type VARCHAR(25) NOT NULL DEFAULT 'NULL',
-  imageURL VARCHAR(50) NULL DEFAULT 'NULL',
-  restaurantURL VARCHAR NULL DEFAULT 'NULL',
-  owner_ID INTEGER NULL DEFAULT NULL,
+  id INTEGER NOT NULL DEFAULT NEXTVAL ('YelpData_seq'),
+  yelp_api_ID VARCHAR(100),
+  address TEXT,
+  ZIP INTEGER,
+  type VARCHAR(25),
+  imageURL VARCHAR(50),
+  restaurantURL VARCHAR,
+  owner_ID INTEGER,
   PRIMARY KEY (id)
 );
 
@@ -87,15 +77,15 @@ CREATE TABLE YelpData (
 -- 
 -- ---
 
-DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Users cascade;
 		
-CREATE SEQUENCE Users_seq;
+CREATE SEQUENCE IF NOT EXISTS Users_seq;
 
 CREATE TABLE Users (
-  id INTEGER NULL DEFAULT NEXTVAL ('Users_seq') DEFAULT NULL,
-  login VARCHAR(30) NOT NULL,
-  password VARCHAR(60) NOT NULL,
-  cheapitem_id INTEGER NULL DEFAULT NULL,
+  id INTEGER NOT NULL DEFAULT NEXTVAL ('Users_seq'),
+  login VARCHAR(30),
+  password VARCHAR(60),
+  cheapitem_id INTEGER,
   PRIMARY KEY (id)
 );
 
@@ -107,28 +97,3 @@ ALTER TABLE Deals ADD FOREIGN KEY (yelp_ID) REFERENCES YelpData (id);
 ALTER TABLE CheapItems ADD FOREIGN KEY (yelp_ID) REFERENCES YelpData (id);
 ALTER TABLE YelpData ADD FOREIGN KEY (owner_ID) REFERENCES Owners (id);
 ALTER TABLE Users ADD FOREIGN KEY (cheapitem_id) REFERENCES CheapItems (id);
-
--- ---
--- Table Properties
--- ---
-
--- ALTER TABLE `Deals` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `CheapItems` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Owners` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `YelpData` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- ---
--- Test Data
--- ---
-
--- INSERT INTO `Deals` (`id`,`yelp_ID`,`price`,`dealName`,`description`,`imageURL`,`startTime`,`endTime`) VALUES
--- ('','','','','','','','');
--- INSERT INTO `CheapItems` (`id`,`yelp_ID`,`price`,`menuItem`,`imageURL`,`description`) VALUES
--- ('','','','','','');
--- INSERT INTO `Owners` (`id`,`login`,`password`) VALUES
--- ('','','');
--- INSERT INTO `YelpData` (`id`,`address`,`ZIP`,`type`,`imageURL`,`restaurantURL`,`owner_ID`) VALUES
--- ('','','','','','','');
--- INSERT INTO `Users` (`id`,`login`,`password`,`cheapitem_id`) VALUES
--- ('','','','');
